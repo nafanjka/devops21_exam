bb0a4d8c12b151bdcc6bf174b031b219eba6151f
nafanjka <stas.hrytsyk@gmail.com>
2022-08-11 20:12:05 +0300

    remove 443 port

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -8,12 +8,6 @@ resource "aws_security_group" "sg_80" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = "443"
-    to_port     = "443"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
     from_port   = "22"
     to_port     = "22"
     protocol    = "tcp"
